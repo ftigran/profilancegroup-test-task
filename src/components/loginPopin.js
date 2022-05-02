0c40410a62e06d71309b538a493bb8935923239a
Tigran Frangyan <ftigran1@gmail.com>
2022-05-02 22:34:16 +0300

    add sign in mechanic

--- src/components/loginPopin.js
+++ src/components/loginPopin.js
@@ -3,17 +3,36 @@ import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import { useSelector, useDispatch } from "react-redux";
 import "./loginPopin.scss";
-import { selectUserLogin, loggout } from "../features/user/userSlice";
+import { selectUserLogin, signIn } from "../features/user/userSlice";
+import { users } from "../features/user/users";
 
 export const LoginPopin = () => {
     const dispatch = useDispatch();
 
-    const [isOpenned, setIsOpenned] = React.useState(true);
+    const [isOpenned, setIsOpenned] = React.useState(false);
+    const [isHasError, setIsHasError] = React.useState(false);
     const showPopin = () => {
         setIsOpenned(true)
     }
     const closePopin = () => {
         setIsOpenned(false)
+    }
+
+    const loginRef = React.useRef();
+    const passwordRef = React.useRef();
+    const loginUser = () => {
+        const login = loginRef.current.value;
+        const password = passwordRef.current.value;
+        const user =users[login];
+
+        if (user && user.password === password){
+            dispatch(signIn({
+                login, type: user.type
+            }))
+            return;
+        }
+        setIsHasError(true)
+        console.log(login, user)
     }
 
     return (
@@ -25,15 +44,18 @@ export const LoginPopin = () => {
                 <button className="popin__close" onClick={closePopin}>X</button>
                 <h1>Войти</h1>
                 <div>
-                    <label for='login'>Логин</label>
-                    <input id="login"/>
+                    <label htmlFor='login'>Логин</label>
+                    <input ref={loginRef}  id="login"/>
                 </div>
                 <div>
-                    <label for='password' type="password">Пароль</label>
-                    <input id="password"/>
+                    <label htmlFor='password' type="password">Пароль</label>
+                    <input ref={passwordRef} id="password"/>
                 </div>
+                {isHasError&& <div>
+                    <span className="login__error">Пользователь не найден!</span>
+                </div>}
                 <div>
-                    <button className="login__button">Войти</button>
+                    <button className="login__button" onClick={loginUser}>Войти</button>
                 </div>
             </div>
         </div>}    
---
+++ src/features/user/users.js
@@ -0,0 +1,12 @@
+import { admin, basic } from "./userTypes";
+
+export const users = {
+    ftigran: {
+        password: '123',
+        type: basic
+    },
+    admin: {
+        password: 'admin',
+        type: admin
+    }
+}
